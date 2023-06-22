bd6c65592905b03a56fb9a9d60c7de8e20f78b91
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-06-22 13:40:20 +0530

    added computer model

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { styles } from "../style";
 import { ComputersCanvas } from "./canvas";
 
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} inset-0 absolute top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} inset-0 absolute top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -22,8 +22,25 @@ const Hero = () => {
             web apps.
           </p>
         </div>
-      </div>
+      </div><br />
       <ComputersCanvas />
+      <div className="absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType : 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            >
+
+            </motion.div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
